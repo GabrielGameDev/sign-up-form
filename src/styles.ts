c6da185f0c0ba79d43ee8009cd91d7ed8044dedf
GabrielGameDev <gabrielgamedev@gmail.com>
2023-02-01 15:01:07 -0300

    Body

--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
-export const Input = styled.input`
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 8px;
-    font-size: 16px;
-    width: 100%;
-`;
+const colors = {
+    red: 'hsl(0, 100%, 74%)', 
+    green: 'hsl(154, 59%, 51%)',
+    blue: 'hsl(248, 32%, 49%)',
+    darkBlue: 'hsl(249, 10%, 26%)' ,
+    grayishBlue: 'hsl(246, 25%, 77%)',
+};
+
+export const Body = styled.div`
+    background-color: ${colors.red};
+    background-image: url('../images/bg-intro-desktop.png');
+    height: 100vh;
+    width: 100vw;
+    `;
+
