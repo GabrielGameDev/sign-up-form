e93d21774f6284e06ddda678d345f12245e7708e
GabrielGameDev <gabrielgamedev@gmail.com>
2023-02-01 13:51:33 -0300

    Commit inicial

---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 8px;
+    font-size: 16px;
+    width: 100%;
+`;
